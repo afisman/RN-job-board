9ef804bb29d2861a5705dba1e9d0195eb7245a97
afisman <afisman@gmail.com>
2024-07-04 12:34:25 +0200

    worked on search component

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
-import { View, Text } from 'react-native'
-
-import styles from './popularjobs.style'
+import { useState } from 'react';
+import { View, Text, Pressable, FlatList, ActivityIndicator } from 'react-native';
+import styles from './popularjobs.style';
+import { useRouter } from 'expo-router';
+import { COLORS, SIZES } from '../../../constants';
+import PopularJobCard from '../../common/cards/popular/PopularJobCard';
 
 const Popularjobs = () => {
+  const router = useRouter();
+
   return (
     <View>
       <Text>Popularjobs</Text>
