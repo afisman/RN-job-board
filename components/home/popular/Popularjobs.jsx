2b3e3cc4c7b3902ef3873e74e804e123e973fd8a
afisman <afisman@gmail.com>
2024-07-05 13:10:11 +0200

    worked on popularjobs and data from hook response

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -9,7 +9,7 @@ import useFetch from '../../../hooks/useFetch';
 
 const Popularjobs = () => {
   const router = useRouter();
-  const { data, isLoading, error } = useFetch('search', { query: 'React developer', num_pages: 1 });
+  const { data, isLoading, error } = useFetch('search', { query: 'React developer', num_pages: 10 });
 
   return (
     <View style={styles.container}>
--- constants/images.js
+++ constants/images.js
@@ -1,4 +1,4 @@
-import profile from "../assets/images/kemal.jpg";
+import profile from "../assets/images/selfie.jpg";
 
 export default {
   profile,
