d3e30790b1227acb8039dab5b3d0c234c93d3351
afisman <afisman@gmail.com>
2024-07-05 12:26:30 +0200

    started on useFetch hook

--- hooks/useFetch.js
+++ hooks/useFetch.js
@@ -1,8 +1,23 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { RAPID_API_KEY } from '@env';
 
-const useFetch = () => {
+const rapidApiKey = RAPID_API_KEY;
+
+const useFetch = (endpoint, query) => {
     const [data, setData] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState(null);
+
+    const options = {
+        method: 'GET',
+        url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+        params: {
+            ...query
+        },
+        headers: {
+            'x-rapidapi-key': rapidApiKey,
+            'x-rapidapi-host': 'jsearch.p.rapidapi.com'
+        }
+    };
 }
