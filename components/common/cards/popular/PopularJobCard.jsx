9750b7fc6d2a7c1bb134e1ec7cd40e895d2b5791
afisman <afisman@gmail.com>
2024-07-06 13:02:05 +0200

    worked on popular job card

--- components/common/cards/popular/PopularJobCard.jsx
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Pressable, Image } from 'react-native'
 
 import styles from './popularjobcard.style'
 
-const PopularJobCard = () => {
+const PopularJobCard = ({ item, selectedJob, handleCardPress }) => {
   return (
-    <View>
-      <Text>PopularJobCard</Text>
-    </View>
+    <Pressable
+      style={styles.container(selectedJob, item)}
+      onPress={() => handleCardPress(item)}
+    >
+      <Pressable
+        style={styles.logoContainer(selectedJob, item)}
+      >
+        <Image
+          source={{ uri: item.employer.logo }}
+          resizeMode='contain'
+          style={styles.logoImage}
+        />
+      </Pressable>
+    </Pressable>
   )
 }
 
